45a28b4a012efc2dafac198f3532d72af3623bab
abrarShariar <shariarabrar@gmail.com>
2017-05-19 15:18:33 +0600

    Minor changes

--- src/app/general/general-routing.module.ts
+++ src/app/general/general-routing.module.ts
@@ -1,11 +1,16 @@
 import {NgModule} from '@angular/core';
 import {RouterModule} from '@angular/router';
 import {KqProductCreateComponent} from './products/kq-product-create/kq-product-create.component';
+import {KqProductListComponent} from './products/kq-product-list/kq-product-list.component';
 
 const generalRoutes = [
   {
     path: 'admin/product/create',
     component: KqProductCreateComponent,
+  },
+  {
+    path: 'admin/product/list',
+    component: KqProductListComponent
   }
 ];
 @NgModule({
--- src/app/general/general.module.ts
+++ src/app/general/general.module.ts
@@ -2,7 +2,7 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {GeneralRoutingModule} from './general-routing.module';
 import {KqProductCreateComponent} from './products/kq-product-create/kq-product-create.component';
-import { KqProductListComponent } from './products/kq-product-list/kq-product-list.component';
+import {KqProductListComponent} from './products/kq-product-list/kq-product-list.component';
 
 @NgModule({
   imports: [
